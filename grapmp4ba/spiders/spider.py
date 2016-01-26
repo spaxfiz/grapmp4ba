a91f5fbbe76f57f9c06227b8deb6cb3a1f4be600
SpaxFiz <343558176@qq.com>
2016-01-26 12:03:23 +0800

    test

--- grapmp4ba/spiders/spider.py
+++ grapmp4ba/spiders/spider.py
@@ -4,6 +4,7 @@ import scrapy
 
 class MovieSpider(scrapy.Spider):
     name = 'mp4ba'
+    allowed_domains = ["www.mp4ba.com"]
     start_url = [
         'http://www.mp4ba.com/index.php?sort_id=2',
         'http://www.mp4ba.com/index.php?sort_id=3'
