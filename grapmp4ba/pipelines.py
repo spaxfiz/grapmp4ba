171d0a2d495677ff26a155f5fec09b9550f96b3d
SpaxFiz <343558176@qq.com>
2016-01-28 11:22:11 +0800

    fix bug

--- grapmp4ba/pipelines.py
+++ grapmp4ba/pipelines.py
@@ -20,7 +20,9 @@ class Grapmp4BaPipeline(object):
 
         definition = pattern.match(item['title']).group() if pattern.match(item['title']) else None
         hashcode = pattern2.match(item['link']).group() if pattern2.match(item['link']) else None
-        
+        print definition
+        print hashcode
+        print detail
         movie = Movie(title=item['title'],
                       date_id=time.strftime('%Y%m%d'),
                       link=item['link'],
